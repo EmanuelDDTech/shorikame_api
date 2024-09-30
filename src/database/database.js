a7e61b718a75ae38ae7970bb2915eb1fcee3f41d
EmanuelDDTech <emanuel@ddtech.mx>
2024-09-29 20:58:27 -0600

    test

--- src/database/database.js
+++ src/database/database.js
@@ -5,7 +5,7 @@ import fs from 'fs';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
-const filePath = join(__dirname, '/../../certificados', 'us-east-2-bundle.pem');
+const filePath = join(__dirname, '/../certificados', 'us-east-2-bundle.pem');
 
 export const sequelize = new Sequelize(
   process.env.DB_DATABASE,
