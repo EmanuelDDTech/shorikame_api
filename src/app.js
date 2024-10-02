1f280f4785f781d19bd61434a6dcb09248399fbf
EmanuelDDTech <emanuel@ddtech.mx>
2024-10-01 22:49:27 -0600

    Cors setup.

--- src/app.js
+++ src/app.js
@@ -1,14 +1,33 @@
 import express from 'express';
-import userRoutes from './routes/users.routes.js';
-import productRoutes from './routes/product.routes.js';
 import morgan from 'morgan';
+import cors from 'cors';
+import authRoutes from './routes/auth.routes.js';
+import productRoutes from './routes/product.routes.js';
 
 const app = express();
 
 app.use(morgan('dev'));
 app.use(express.json());
 
-app.use(userRoutes);
+const whitelist = [process.env.FRONTEND_URL];
+
+if (process.argv[2] === '--insomnia') {
+  whitelist.push(undefined);
+}
+
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Error de CORS'));
+    }
+  },
+};
+
+app.use(cors(corsOptions));
+
+app.use('/auth', authRoutes);
 app.use(productRoutes);
 
 export default app;
