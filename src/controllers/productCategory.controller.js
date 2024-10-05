c65ee4f316e7d9061016169ece0924a5b9aaa3e1
EmanuelDDTech <emanuel@ddtech.mx>
2024-10-04 18:17:12 -0600

    Product category routes and controller.

---
+++ src/controllers/productCategory.controller.js
@@ -0,0 +1,71 @@
+import { ProductCategory } from '../models/ProductCategory.js';
+
+const createCategory = async (req, res) => {
+  if (Object.values(req.body).includes('')) {
+    const error = new Error('Todos los campos son obligatorios');
+    return res.status(400).json({ msg: error.message });
+  }
+
+  const { name } = req.body;
+
+  try {
+    const newCategory = await ProductCategory.create({ name });
+    return res.json({ msg: 'Categoría creada correctamente' });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const getCategories = async (req, res) => {
+  const categories = await ProductCategory.findAll();
+  return res.json(categories);
+};
+
+const getCategoryById = async (req, res) => {
+  const { id } = req.params;
+  const category = await ProductCategory.findOne({ where: { id } });
+
+  if (!category) {
+    const error = new Error('La categoría no existe');
+    return res.status(401).json({ msg: error.message });
+  }
+
+  return res.json(category);
+};
+
+const updateCategory = async (req, res) => {
+  const { id } = req.params;
+  const category = await ProductCategory.findOne({ where: { id } });
+
+  if (!category) {
+    const error = new Error('La categoría no existe');
+    return res.status(401).json({ msg: error.message });
+  }
+
+  category.name = req.body.name || category.name;
+
+  try {
+    await category.save();
+    res.json({ msg: 'Categoría actualizada correctamente' });
+  } catch (error) {
+    return res.status(500).json({ msg: error.message });
+  }
+};
+
+const deleteCategory = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    await ProductCategory.destroy({
+      where: {
+        id,
+      },
+    });
+
+    return res.sendStatus(204);
+  } catch (error) {
+    return res.status(500).json({ msg: error.message });
+  }
+};
+
+export { createCategory, getCategories, getCategoryById, updateCategory, deleteCategory };
---
+++ src/routes/category.routes.js
@@ -0,0 +1,16 @@
+import { Router } from 'express';
+import {
+  createCategory,
+  deleteCategory,
+  getCategories,
+  getCategoryById,
+  updateCategory,
+} from '../controllers/productCategory.controller.js';
+
+const router = Router();
+
+router.route('/').get(getCategories).post(createCategory);
+
+router.route('/:id').get(getCategoryById).put(updateCategory).delete(deleteCategory);
+
+export default router;
