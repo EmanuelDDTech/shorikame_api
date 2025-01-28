23d5ffddcdeb0a563dacd50efb64d72e9a374711
EmanuelDDTech <emanuel@ddtech.mx>
2025-01-27 22:43:11 -0600

    feat(campaign): add campaign routes

---
+++ src/routes/campaign.routes.js
@@ -0,0 +1,20 @@
+import { Router } from 'express';
+import {
+  createCampaign,
+  deleteCampaign,
+  getCampaignAll,
+  getCampaignById,
+  updateCampaign,
+} from '../controllers/campaign.controller.js';
+import authMiddleware from '../middleware/authMiddleware.js';
+
+const router = Router();
+
+router.route('/').get(authMiddleware, getCampaignAll).post(authMiddleware, createCampaign);
+router
+  .route('/:id')
+  .get(authMiddleware, getCampaignById)
+  .put(authMiddleware, updateCampaign)
+  .delete(authMiddleware, deleteCampaign);
+
+export default router;
