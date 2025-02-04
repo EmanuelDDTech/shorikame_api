90e9422577b41409be676a9eda0310b51f944384
EmanuelDDTech <emanuel@ddtech.mx>
2025-02-04 00:14:28 -0600

    feat(products): add buscar route

--- src/routes/product.routes.js
+++ src/routes/product.routes.js
@@ -4,13 +4,14 @@ import {
   deleteProduct,
   getProductById,
   getProducts,
+  searchProducts,
   updateProduct,
 } from '../controllers/product.controller.js';
 
 const router = Router();
 
 router.route('/products').get(getProducts).post(createProduct);
-
 router.route('/products/:id').get(getProductById).delete(deleteProduct).put(updateProduct);
+router.route('/buscar').get(searchProducts);
 
 export default router;
