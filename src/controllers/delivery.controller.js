d26e5274251159a487249ad398d3c7d6a64b161e
EmanuelDDTech <emanuel@ddtech.mx>
2025-02-21 20:16:04 -0600

    feat(delivery): add delivery controller

---
+++ src/controllers/delivery.controller.js
@@ -0,0 +1,56 @@
+import { Op } from 'sequelize';
+import { DeliveryCarrier } from '../models/DeliveryCarrier.js';
+import { DeliveryPriceRule } from '../models/DeliveryPriceRule.js';
+import { Product } from '../models/Product.js';
+
+const findAvailable = async (req, res) => {
+  const { weight, zipCode } = req.query;
+  const serviceLevels = ['delivery'];
+  let freeDelivery = {};
+
+  try {
+    const deliveries = await DeliveryCarrier.findAll({
+      where: {
+        free_over: false,
+        servicelevel: serviceLevels,
+      },
+      attributes: { exclude: ['createdAt', 'updatedAt'] },
+      include: [
+        {
+          model: DeliveryPriceRule,
+          required: true,
+          order: [['max_value', 'ASC']],
+          where: {
+            max_value: {
+              [Op.gte]: weight,
+            },
+          },
+          attributes: { exclude: ['createdAt', 'updatedAt'] },
+        },
+      ],
+    });
+
+    if (weight <= 0) {
+      freeDelivery = await findFreeDelivery();
+      deliveries.push(freeDelivery);
+    }
+
+    return res.json(deliveries);
+  } catch (error) {
+    return res.status(500).json({ msg: error.message });
+  }
+};
+
+const findFreeDelivery = async () => {
+  try {
+    return await DeliveryCarrier.findOne({
+      where: {
+        free_over: true,
+      },
+    });
+  } catch (error) {
+    return res.status(500).json({ msg: error.message });
+  }
+};
+
+export { findAvailable };
