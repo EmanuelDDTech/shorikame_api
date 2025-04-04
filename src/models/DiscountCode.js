716a0cf19b44f1c06ea55bb469a68fd51074b583
EmanuelDDTech <emanuel@ddtech.mx>
2025-04-04 14:53:11 -0600

    feat(discountCode): create model

---
+++ src/models/DiscountCode.js
@@ -0,0 +1,41 @@
+import { DataTypes } from 'sequelize';
+import { sequelize } from '../database/database.js';
+
+export const DiscountCode = sequelize.define('discount_code', {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  code: {
+    type: DataTypes.STRING(50),
+    allowNull: false,
+  },
+  discount_type: {
+    type: DataTypes.ENUM('fijo', 'porcentaje'),
+    allowNull: false,
+    defaultValue: 'fijo',
+  },
+  discount_value: {
+    type: DataTypes.DECIMAL(10, 2),
+    allowNull: false,
+  },
+  min_purchase: {
+    type: DataTypes.DECIMAL(10, 2),
+    defaultValue: 0,
+  },
+  max_discount: {
+    type: DataTypes.DECIMAL(10, 2),
+  },
+  usage_limit: {
+    type: DataTypes.INTEGER,
+    defaultValue: 1,
+  },
+  times_used: {
+    type: DataTypes.INTEGER,
+    defaultValue: 0,
+  },
+  expires_at: {
+    type: DataTypes.DATE,
+  },
+});
