230cd497f0576a7bd1101eff7c98d242dd4d2dca
EmanuelDDTech <emanuel@ddtech.mx>
2025-06-14 14:20:07 -0600

    feat(sale.routes) add getAdmin route

--- src/routes/sale.routes.js
+++ src/routes/sale.routes.js
@@ -4,12 +4,15 @@ import {
   createOrder,
   getOrderById,
   getOrders,
+  getOrdersAdmin,
   updateOrder,
 } from '../controllers/sale.controller.js';
 
 const router = Router();
 
 router.route('/').get(authMiddleware, getOrders).post(authMiddleware, createOrder);
+router.route('/admin').get(authMiddleware, getOrdersAdmin);
+
 router.route('/:id').get(authMiddleware, getOrderById).put(authMiddleware, updateOrder);
 
 export default router;
