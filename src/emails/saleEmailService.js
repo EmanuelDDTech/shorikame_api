1623557d17295d7f339f1b99e9c6c39fd19a411c
EmanuelDDTech <emanuel@ddtech.mx>
2025-06-15 20:54:43 -0600

    feat(saleEmailService): add update email and add shipping and discount on email

--- src/emails/saleEmailService.js
+++ src/emails/saleEmailService.js
@@ -32,9 +32,16 @@ export async function sendEmailSaleConfirmation(orderData) {
     return;
   }
 
-  console.log(data);
+  console.log('Mensaje enviado', data.id);
+}
 
-  // console.log('Mensaje enviado', info.messageId);
+export async function sendEmailSaleUpdate(saleData) {
+  const { data, error } = await resend.emails.send({
+    from: 'ShoriKameCards <pedidos@shorikamecards.com>',
+    to: [saleData.user.email],
+    subject: `ShoriKameCards - Pedido #${saleData.id} ${saleData.state}`,
+    html: renderUpdateHTML(saleData),
+  });
 }
 
 function renderHTML({ user, order, items }) {
@@ -48,6 +55,22 @@ function renderHTML({ user, order, items }) {
                               <td>${item.price * item.quantity}</td>
                           </tr>`;
   });
+
+  htmlProductRows += `<tr>
+                              <td>Envío</td>
+                              <td>N/A</td>
+                              <td>${order.amount_shipping}</td>
+                              <td>${order.amount_shipping}</td>
+                          </tr>`;
+
+  if (order.discount_amount > 0) {
+    htmlProductRows += `<tr>
+                              <td>Descuento</td>
+                              <td>N/A</td>
+                              <td>${-order.discount_amount}</td>
+                              <td>${-order.discount_amount}</td>
+                          </tr>`;
+  }
 
   const htmlEmail = `<head>
               <meta charset="UTF-8">
@@ -121,6 +144,129 @@ function renderHTML({ user, order, items }) {
                           ${htmlProductRows}
                       </table>
                       <p><strong>Total: ${order.amount_total}</strong></p>
+                      <p>Si tienes dudas, contáctanos en <a href="mailto:shorikamecards@gmail.com">shorikamecards@gmail.com</a>.</p>
+                      <p>Gracias por confiar en <strong>ShoriKameCards</strong> 🚀</p>
+                  </div>
+                  <div class="footer">
+                      <p>&copy; 2025 ShoriKamenCards. Todos los derechos reservados.</p>
+                  </div>
+              </div>
+          </body>`;
+
+  return htmlEmail;
+}
+
+function renderUpdateHTML(saleData) {
+  let htmlProductRows = ``;
+
+  let updateStatusTitle = '';
+  let updateStatusDescription = '';
+
+  if (saleData.state === 'completado') {
+    updateStatusTitle = `Pedido #${saleData.id} completado`;
+    updateStatusDescription = `Tu pedido #${saleData.id} ha sido entregado.`;
+  } else if (saleData.state === 'pendiente') {
+    updateStatusTitle = `Pedido #${saleData.id} pendiente`;
+    updateStatusDescription = `Tu pedido #${saleData.id} está siendo procesado.`;
+  }
+
+  saleData.sale_carts.forEach((item) => {
+    htmlProductRows += `<tr>
+                              <td>${item.product.name}</td>
+                              <td>${item.quantity}</td>
+                              <td>${item.price_unit}</td>
+                              <td>${item.price_unit * item.quantity}</td>
+                          </tr>`;
+  });
+
+  htmlProductRows += `<tr>
+                              <td>${saleData.delivery_carrier.name}</td>
+                              <td>N/A</td>
+                              <td>${saleData.amount_shipping}</td>
+                              <td>${saleData.amount_shipping}</td>
+                          </tr>`;
+
+  if (saleData.discount_amount > 0) {
+    htmlProductRows += `<tr>
+                              <td>Descuento</td>
+                              <td>N/A</td>
+                              <td>${-saleData.discount_amount}</td>
+                              <td>${-saleData.discount_amount}</td>
+                          </tr>`;
+  }
+
+  const htmlEmail = `<head>
+              <meta charset="UTF-8">
+              <meta name="viewport" content="width=device-width, initial-scale=1.0">
+              <title>Actualización de Pedido #${saleData.id}</title>
+              <style>
+                  body {
+                      font-family: Arial, sans-serif;
+                      background-color: #f4f4f4;
+                      margin: 0;
+                      padding: 0;
+                  }
+                  .container {
+                      width: 100%;
+                      max-width: 600px;
+                      margin: 20px auto;
+                      background: #ffffff;
+                      padding: 20px;
+                      border-radius: 10px;
+                      box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+                  }
+                  .header {
+                      text-align: center;
+                      margin-bottom: 20px;
+                  }
+                  .header img {
+                      max-width: 200px;
+                  }
+                  .content p {
+                      font-size: 16px;
+                      color: #333;
+                  }
+                  .order-details {
+                      width: 100%;
+                      border-collapse: collapse;
+                      margin-top: 20px;
+                  }
+                  .order-details th, .order-details td {
+                      border: 1px solid #ddd;
+                      padding: 10px;
+                      text-align: left;
+                  }
+                  .order-details th {
+                      background-color: #f8f8f8;
+                  }
+                  .footer {
+                      text-align: center;
+                      margin-top: 20px;
+                      font-size: 14px;
+                      color: #777;
+                  }
+              </style>
+          </head>
+          <body>
+              <div class="container">
+                  <div class="header">
+                      <img src="https://firebasestorage.googleapis.com/v0/b/shorikame-7d2b4.appspot.com/o/assets%2Fkame_01.png?alt=media&token=9c1e6563-2477-4960-8ac0-efffbd5e0634" alt="Shorikame Logo">
+                  </div>
+                  <div class="content">
+                      <h2>${updateStatusTitle}</h2>
+                      <p>Hola ${saleData.user.name},</p>
+                      <p>${updateStatusDescription}</p>
+                      <h3>Detalles del Pedido:</h3>
+                      <table class="order-details">
+                          <tr>
+                              <th>Producto</th>
+                              <th>Cantidad</th>
+                              <th>Precio</th>
+                              <th>Subtotal</th>
+                          </tr>
+                          ${htmlProductRows}
+                      </table>
+                      <p><strong>Total: ${saleData.amount_total}</strong></p>
                       <p>Si tienes dudas, contáctanos en <a href="mailto:shorikamecards@gmail.com">shorikamecards@gmail.com</a>.</p>
                       <p>Gracias por confiar en <strong>ShoriKameCards</strong> 🚀</p>
                   </div>
