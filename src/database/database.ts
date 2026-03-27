415defc4623a13529175d4368315d363272899fa
EmanuelDDTech <emanuel@ddtech.mx>
2026-03-27 01:03:58 -0600

    feat(supabase): configure supabase

--- src/database/database.ts
+++ src/database/database.ts
@@ -1,24 +1,26 @@
-import Sequelize from 'sequelize';
+import { Sequelize } from 'sequelize';
 import pg from 'pg';
 import { fileURLToPath } from 'url';
 import { dirname, join } from 'path';
 import fs from 'fs';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-const filePath = join(__dirname, '/../certificados', 'us-east-2-bundle.pem');
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = dirname(__filename);
+// const filePath = join(__dirname, '/../certificados', 'us-east-2-bundle.pem');
 
 export const sequelize = new Sequelize(
-  process.env.DB_DATABASE,
-  process.env.DB_USER,
-  process.env.DB_PASS,
+  process.env.DB_DATABASE!,
+  process.env.DB_USER!,
+  process.env.DB_PASS!,
   {
     host: process.env.DB_HOST,
     dialect: 'postgres',
     dialectOptions: {
       ssl: {
-        rejectUnauthorized: true,
-        ca: fs.readFileSync(filePath).toString(),
+        // rejectUnauthorized: true,
+        // ca: fs.readFileSync(filePath).toString(),
+        require: true,
+        rejectUnauthorized: false,
       },
     },
     dialectModule: pg,
